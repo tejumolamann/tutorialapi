c0d9922bb1fc7122435cffeca854631957e32cb6
Tejumola Mann <tejumolamann@gmail.com>
2026-03-18 08:37:51 +0100

    Added logback.xml configuration

--- tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -2,9 +2,14 @@ package com.tutorialapi.server;
 
 import org.eclipse.jetty.http.HttpScheme;
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TutorialApiServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TutorialApiServer.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Starting Tutorial API Server");
 
         HttpConfiguration httpsConfiguration = new HttpConfiguration();
         httpsConfiguration.setSecureScheme(HttpScheme.HTTPS.asString());
