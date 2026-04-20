226a8b15df79c405e9cd1d6e91517c5e06d3b58e
Tejumola Mann <tejumolamann@gmail.com>
2026-04-20 16:41:08 +0100

    Add CRUD resources for managing Todo items
    - Implement `/v1/lists/{listId}/items` endpoints for `GET`, `POST`, `PUT`, and `DELETE` operations.
    - Introduce separate resource classes for handling individual Todo item operations.
    - Leverage `ServiceFactory` to manage Todo item data through the new resources.

---
+++ tutorialapi-rest/src/main/java/com/tutorialapi/rest/resource/v1/items/GetAllTodoItemsResource.java
@@ -0,0 +1,32 @@
+package com.tutorialapi.rest.resource.v1.items;
+
+import com.tutorialapi.db.ServiceFactory;
+import com.tutorialapi.model.TodoItem;
+import com.tutorialapi.model.user.RapidApiPrincipal;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
+
+import java.util.List;
+
+@Path("/v1/lists/{listId}/items")
+public class GetAllTodoItemsResource {
+    private final ServiceFactory serviceFactory;
+
+    @Inject
+    public GetAllTodoItemsResource(ServiceFactory serviceFactory) {
+        this.serviceFactory = serviceFactory;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<TodoItem> getTodoItems(@Context SecurityContext securityContext, @PathParam("listId") String listId) {
+        RapidApiPrincipal principal = (RapidApiPrincipal) securityContext.getUserPrincipal();
+        return serviceFactory.getTodoItemService().getAll(principal, listId);
+    }
+}
